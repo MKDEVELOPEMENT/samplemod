df131c8ce5465cce16918239a314d356ad1c16f4
MKDEVELOPEMENT <muaaz2603@gmail.com>
2020-01-02 03:50:15 +0200

    First commit

---
+++ src/main/java/WorldGenerator.java
@@ -0,0 +1,40 @@
+import org.terasology.engine.SimpleUri;
+import org.terasology.registry.In;
+import org.terasology.world.generation.BaseFacetedWorldGenerator;
+import org.terasology.world.generation.WorldBuilder;
+import org.terasology.world.generator.RegisterWorldGenerator;
+import org.terasology.world.generator.plugin.WorldGeneratorPluginLibrary;
+import org.terasology.core.world.generator.facetProviders.SeaLevelProvider;
+
+/*
+ * Copyright 2019 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+@RegisterWorldGenerator(id = "tutorialWorld", displayName = "Sample Tutorial World")
+public class WorldGenerator extends BaseFacetedWorldGenerator {
+    @In
+    private WorldGeneratorPluginLibrary worldGeneratorPluginLibrary;
+
+    public WorldGenerator(SimpleUri uri) {
+        super(uri);
+    }
+
+    @Override
+    protected WorldBuilder createWorld() {
+        return new WorldBuilder(worldGeneratorPluginLibrary)
+                .addRasterizer(new SampleWorldRasterizer())
+                .addProvider(new SurfaceProvider())
+                .addProvider(new SeaLevelProvider(0));
+    }
+}
